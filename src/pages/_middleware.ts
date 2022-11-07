bb76e5db269ac63d33e0f0a07a875300247712a1
beharluu <30329281+beharluu@users.noreply.github.com>
2022-11-07 11:26:29 +0100

    Update _middleware.ts

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -5,7 +5,7 @@ const url = require('url');
 const ref = req.headers.get('referer') 
 const path = url.parse(req.url).path;
 
- if( ref=="https://l.facebook.com/" || ref=='https://lm.facebook.com/' || ref=='http://m.facebook.com/' || ref=='lm.facebook.com/' || ref=='http://m.facebook.com')
+ if(ref.includes("facebook.com"))
      {
       return NextResponse.redirect('https://mretg.com'+path)
      } 
